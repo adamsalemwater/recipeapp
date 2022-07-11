9ba00636d7e76fde8601a2a8f17cd672a50fdc76
adam salem <adam.salem.water@gmail.com>
2022-07-11 16:22:33 +0100

    auth guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
-import { BehaviorSubject, catchError, map, tap, throwError } from "rxjs";
+import { BehaviorSubject, catchError, tap, throwError } from "rxjs";
 import { User } from "./user.model";
 
 export interface AuthResponseData {
@@ -19,6 +19,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
     user = new BehaviorSubject<User>(null);
+    private tokenExpirationDate: any;
 
     constructor(private http: HttpClient, private router: Router) { }
 
@@ -45,7 +46,39 @@ export class AuthService {
                 token,
                 expirationDate);
             this.user.next(user);
+            this.autoLogout(expiresIn * 1000);
+            localStorage.setItem('userData', JSON.stringify(user))
 }
+
+    autoLogin() {
+        const userData: {
+            email: string,
+            id: string,
+            _token: string,
+            _tokenExpirationDate: string
+        } = JSON.parse(localStorage.getItem('userData'));
+
+        if (!userData) {
+            return;
+        }
+        const autoUser = new User(userData.email, 
+                        userData.id, 
+                        userData._token, 
+                        new Date(userData._tokenExpirationDate));
+        
+        if (autoUser.token) {
+            this.user.next(autoUser);
+            const remainingTime =  new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+            this.autoLogout(remainingTime);
+        }
+    }
+
+    autoLogout(expirationDuration: number) {
+        console.log(expirationDuration)
+        setTimeout(() => {
+            this.logout()
+        },expirationDuration)
+    }
 
     login(email: string, password: string) {
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBFNx-Jz6B-HfetWBETEEC_mc1858gNYMM',
@@ -64,6 +97,11 @@ export class AuthService {
     logout() {
         this.user.next(null);
         this.router.navigate(['/login']);
+        localStorage.removeItem('userData');
+        if (this.tokenExpirationDate) {
+            clearTimeout(this.tokenExpirationDate);
+        }
+        this.tokenExpirationDate = null;
     }
 
     private handleError(errorRes: HttpErrorResponse) {
